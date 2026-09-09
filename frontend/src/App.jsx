e7533628db89d2693d60816c9b7436c2696f24ab
Shadow-inmutable <bberrio2301@gmail.com>
2026-09-09 08:49:37 -0500

    fix(frontend): actualizar rutas y acceso por roles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
-import register from './pages/register';
+import Register from './pages/register';
 import Mapa from './pages/Mapa';
 import Dashboard from './pages/Dashboard';
 import ParadasForm from './pages/ParadasForms';
 import RutasForm from './pages/RutasForm';
 import ZonasForm from './pages/ZonasForms';
+import User from './pages/User';
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
@@ -16,18 +17,30 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        {/* RUTAS PÚBLICAS */}
+        {/* ===============
+          RUTAS PÚBLICAS
+        ======================*/}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<register />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/mapa" element={<Mapa />} />
 
-        {/* RUTAS PROTEGIDAS PARA EL GESTOR */}
-        <Route element={<ProtectedRoute allowedRoles={['gestor']} />}>
+        {/* ==================================
+          RUTAS PROTEGIDAS PARA EL ADMINISTRADOR Y GESTOR
+        =======================================*/}
+        <Route element={<ProtectedRoute allowedRoles={['admin', 'gestor']} />}>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/paradas" element={<ParadasForm />} />
           <Route path="/rutas" element={<RutasForm />} />
           <Route path="/zonas" element={<ZonasForm />} />
+        </Route>
+
+
+        {/* =========================================
+                RUTA EXCLUSIVA DEL ADMIN
+         ========================================= */}
+        <Route element={<ProtectedRoute allowedRoles={['admin']}/>}>
+          <Route path="/usuarios"element={<User />}/>
         </Route>
 
         {/* Redirección por defecto si la URL no existe */}
